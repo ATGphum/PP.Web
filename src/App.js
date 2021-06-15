80c4ee96433845f80d1a7eda27b544c137820579
PhumLWC <jeremyvuong.dshs@gmail.com>
2021-06-15 16:10:51 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -8,33 +8,33 @@ function App() {
 
   const callAPI = () => {
     fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => setApiResponse(res));
+      .then(res => res.text())
+      .then(res => setApiResponse(res));
   }
 
   useEffect(() => {
     callAPI();
-});
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-        </header>
-        <p className="App-intro">;{apiResponse}</p>
-      </div>
-    );
-  
+      </header>
+      <p className="App-intro">;{apiResponse}</p>
+    </div>
+  );
+
 }
 
 export default App;
